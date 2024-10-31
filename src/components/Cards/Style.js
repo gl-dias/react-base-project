d1f0684cb7439c2039b395641c09aa508258bb58
Guilherme Batista <guilhermedias.batista@gmail.com>
2024-10-30 21:45:39 -0300

    nova branch e começo na alteração da media queries

--- src/components/Cards/Style.js
+++ src/components/Cards/Style.js
@@ -3,10 +3,8 @@ import styled from "styled-components";
 const Personalizacao = styled.div`
     margin-left: 85px;
     margin-right: 85px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    flex-wrap: wrap;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
     margin-top: 15px;
     gap: 5px 10px;
 `
