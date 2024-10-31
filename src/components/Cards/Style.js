ecbb43f3cd388a65a7a89c3a219ce82aa4b51d15
Guilherme Batista <guilhermedias.batista@gmail.com>
2024-10-30 22:55:40 -0300

    media queries do footer, header, cards(colocando quantidade de cards para especificos tamanhos de tela)

--- src/components/Cards/Style.js
+++ src/components/Cards/Style.js
@@ -1,11 +1,27 @@
 import styled from "styled-components";
 
 const Personalizacao = styled.div`
-    margin-left: 85px;
-    margin-right: 85px;
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    margin-top: 15px;
-    gap: 5px 10px;
-`
-export {Personalizacao}
+  margin-left: 85px;
+  margin-right: 85px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  margin-top: 15px;
+  gap: 5px 10px;
+
+  @media (max-width: 1024px) {
+    grid-template-columns: repeat(3, 1fr);
+    justify-content: center;
+  }
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+    justify-content: center;
+  }
+
+  @media (max-width: 490px) {
+    grid-template-columns: repeat(1, 1fr);
+    justify-content: center;
+  }
+`;
+
+export { Personalizacao };
