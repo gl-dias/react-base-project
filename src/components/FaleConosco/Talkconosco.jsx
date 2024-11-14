5891fdd1b3565415200cfe627f20d540977391b0
Arthur-azeredo <arthurazeredo8@gmail.com>
2024-11-14 00:42:45 -0300

    coom it

---
+++ src/components/FaleConosco/Talkconosco.jsx
@@ -0,0 +1,11 @@
+import { Fale } from "./style";
+
+const Talkconosco = () => (
+    <Fale> 
+        <div className="FC">
+            <h2>Fale conosco</h2>
+        </div>
+    </Fale>
+);
+
+export default Talkconosco ;
